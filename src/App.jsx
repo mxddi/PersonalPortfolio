9f9ddefa477ac83d9e8585fc21518f6278b967d8
Maddi <57687290+mxddi@users.noreply.github.com>
2023-05-13 14:38:21 -0700

    Added siteprops personal info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Maddi Gregory",
+  title: "Qunatum Physics Major and Software Engineer",
+  email: "maddigregory1@gmail.com",
+  gitHub: "m.xddi",
+  instagram: "m.xddi",
+  linkedIn: "https://www.linkedin.com/in/maddi-g/",
+  //twitter: "microsoft",
+  //youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
